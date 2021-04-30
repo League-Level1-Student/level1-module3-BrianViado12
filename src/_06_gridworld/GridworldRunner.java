e1b137aacf932321c8f84a10ecbb98938d757c6f
Daddy <Daddy@vivs-macpro-13>
2021-04-29 17:24:41 -0700

    did this 4/29/21

--- src/_06_gridworld/GridworldRunner.java
+++ src/_06_gridworld/GridworldRunner.java
@@ -1,5 +1,7 @@
 package _06_gridworld;
 
+import java.awt.Color;
+
 import info.gridworld.actor.Bug;
 import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
@@ -11,10 +13,22 @@ public class GridworldRunner {
 		world.show();
 		Location location = new Location(1, 1);
 		Location location1 = new Location(5, 4);
+		Location location2 = new Location(0,1);
+		Location location3 = new Location(2, 1);
 		Bug bug1 = new Bug();
-		Bug bug2 = new Bug();
+		Bug bug2 = new Bug(Color.blue);
+		Flower flower1 = new Flower();
+		Flower flower2 = new Flower();
+		Flower flower3 = new Flower(Color.red);
 		world.add(location, bug1);
 		world.add(location1, bug2);
+		world.add(location2, flower1);
+		world.add(location3, flower2);
+		for (int i = 0; i < 95; i++) {
+			world.add(world.getRandomEmptyLocation(), flower3);
+		}
+		bug1.turn();
+		bug1.turn();
 		
 	}
 	
--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -11,31 +11,39 @@ import javax.swing.JApplet;
 public class CowTimer {
 
 	/*
-	 * 1. Make a constructor for the CowTimer class that initializes the minutes
+	 * 1. Make a constructor for the CowTimer class that initializes the seconds
 	 * variable
 	 */
-
+		CowTimer(int seconds){
+			this.seconds = seconds;
+		}
 	/* 4. Complete the main method of the CowTimerRunner class */
 
-	private int minutes;
+	private int seconds;
 
-	public void setTime(int minutes) {
-		this.minutes = minutes;
-		System.out.println("Cow time set to " + minutes + " minutes.");
+	public void setTime(int seconds) {
+		this.seconds = seconds;
+		System.out.println("Cow time set to " + seconds + " seconds.");
 	}
 
 	public void start() throws InterruptedException {
 		/*
-		 * 2. Count down the minutes, print the current minute then sleep for the number
-		 * of minutes using Thread.sleep(int milliseconds).
+		 * 2. Count down the seconds, print the current second then sleep for the number
+		 * of seconds using Thread.sleep(int milliseconds).
 		 */
-
+			for (int i = seconds; i >= 0; i--) {
+				System.out.println(i);
+				Thread.sleep(1000);
+			}
+		
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
 		 * from freesound.org, then drag it intothe default package.
 		 */
-
+			if(seconds >= 0) {
+				playSound("moo.wav");
+			}
 	}
 
 	private void playSound(String fileName) {
